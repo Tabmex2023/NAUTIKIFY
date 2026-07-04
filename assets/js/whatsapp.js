7b74c10f7c459cdbc815cec2acb813e5fe72139a
Tabmex2023 <tobiasstuevel@icloud.com>
2026-07-04 10:30:40 -0700

    Update whatsapp.js

--- assets/js/whatsapp.js
+++ assets/js/whatsapp.js
@@ -1,10 +1,15 @@
 document.getElementById("whatsapp").innerHTML = `
+
 <a href="https://wa.me/526731340913"
+
    target="_blank"
+
    class="whatsapp"
+
    aria-label="WhatsApp">
 
-    💬
+    <i class="fa-brands fa-whatsapp"></i>
 
 </a>
+
 `;

2cbf63d14814bbf4c6eaef646f5457a63dcc58bb
Tabmex2023 <tobiasstuevel@icloud.com>
2026-07-04 08:20:33 -0700

    Update header.js

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,1 +1,85 @@
+document.getElementById("header").innerHTML = `
 
+<header>
+
+    <div class="header-container">
+
+        <a href="index.html" class="logo">
+
+            NAUTIK<span>IFY</span>
+
+        </a>
+
+        <nav id="navigation">
+
+            <ul>
+
+                <li>
+
+                    <a href="index.html">Startseite</a>
+
+                </li>
+
+                <li>
+
+                    <a href="charter.html">Charter</a>
+
+                    <div class="dropdown">
+
+                        <a href="mallorca.html">Mallorca</a>
+
+                        <a href="ibiza.html">Ibiza</a>
+
+                        <a href="barcelona.html">Barcelona</a>
+
+                        <a href="cote-azur.html">Côte d'Azur</a>
+
+                        <a href="sardinia.html">Sardinien</a>
+
+                        <a href="athens.html">Athen</a>
+
+                        <a href="mykonos.html">Mykonos</a>
+
+                        <a href="dubrovnik.html">Dubrovnik</a>
+
+                        <a href="malta.html">Malta</a>
+
+                        <a href="kanaren.html">Kanaren</a>
+
+                    </div>
+
+                </li>
+
+                <li>
+
+                    <a href="deals.html">Deals</a>
+
+                </li>
+
+                <li>
+
+                    <a href="about.html">Über uns</a>
+
+                </li>
+
+                <li>
+
+                    <a href="contact.html">Kontakt</a>
+
+                </li>
+
+            </ul>
+
+        </nav>
+
+        <div class="mobile-toggle">
+
+            <span></span>
+
+        </div>
+
+    </div>
+
+</header>
+
+`;

902c134c248d9f7820cbca60ea6c6bac34af09b4
Tabmex2023 <tobiasstuevel@icloud.com>
2026-07-17 15:06:46 -0700

    Update whatsapp.js

--- assets/js/whatsapp.js
+++ assets/js/whatsapp.js
@@ -1,6 +1,6 @@
 document.getElementById("whatsapp").innerHTML = `
 
-<a href="https://wa.me/526731340913"
+<a href="https://wa.me/526731340913?text=Hallo%20Nautikify%2C%0A%0Aich%20interessiere%20mich%20f%C3%BCr%20einen%20Yachtcharter%20und%20h%C3%A4tte%20gerne%20weitere%20Informationen.%0A%0AIch%20freue%20mich%20auf%20Ihre%20R%C3%BCckmeldung.%0A%0AVielen%20Dank!"
 
    target="_blank"
 
